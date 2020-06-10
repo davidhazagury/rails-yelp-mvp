5c0b35b3a4b6d1b3a281d94709d3c66b9f6a57be
David Hernandez Azagury <davidhazagury@gmail.com>
2020-06-10 14:43:36 +0200

    Finished step db, controller. Working on index and show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,15 @@
 class RestaurantsController < ApplicationController
+
+  # Shows the list of all restaurants
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # Shows one restaurant
+
+  def show
+
+  end
+
 end
