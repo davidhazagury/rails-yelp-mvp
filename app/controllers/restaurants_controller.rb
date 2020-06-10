1fae30b8720143e25733072bad3fc4c9b4fdb6f8
David Hernandez Azagury <davidhazagury@gmail.com>
2020-06-10 16:37:23 +0200

    Finished step, method new and create of review, and show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,28 @@
 class RestaurantsController < ApplicationController
 
   # Shows the list of all restaurants
-
   def index
     @restaurants = Restaurant.all
   end
 
   # Shows one restaurant
-
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Get the form and create an instance out of it.
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(rest_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def rest_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.create(rev_params)
+  end
+
+  private
+
+  def rev_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
